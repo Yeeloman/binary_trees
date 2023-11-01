fa3b4153b7fb3ad8f8ab9a45dfc35e3e72080c57
Yandelf00 <zakaria.afyf00@gmail.com>
2023-11-01 02:51:47 +0100

    add

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- *binary_tree_depth - give the size of the tree
+ *binary_tree_size - give the size of the tree
  *@tree : is a pointer to the root node of the tree to measure the size.
  *Return: 0 if null.
  */
@@ -11,5 +11,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-    return (binary_tree_size(tree->right)+binary_tree_size(tree->left)+1);
+	return (binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1);
 }
