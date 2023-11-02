943dff4a687afba2016d42524084f3b6915ea2df
Yeeloman <yami.onlyme@gmail.com>
2023-11-03 00:56:16 +0100

    add some tasks

--- main.c
+++ main.c
@@ -3,30 +3,27 @@
 #include <stdlib.h>
 
 /**
- * print_num - Prints a number
- *
- * @n: Number to be printed
- */
-void print_num(int n) { printf("%d\n", n); }
-
-/**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success, error code on failure
  */
 int main(void) {
   binary_tree_t *root;
 
   root = binary_tree_node(NULL, 98);
-  root->left = binary_tree_node(root, 12);
-  root->right = binary_tree_node(root, 402);
-  root->left->left = binary_tree_node(root->left, 6);
-  root->left->right = binary_tree_node(root->left, 56);
-  root->right->left = binary_tree_node(root->right, 256);
-  root->right->right = binary_tree_node(root->right, 512);
-
+  root->right = binary_tree_node(root, 128);
+  root->right->right = binary_tree_node(root->right, 402);
   binary_tree_print(root);
-  binary_tree_levelorder(root, &print_num);
-  binary_tree_delete(root);
+  printf("Rotate-left %d\n", root->n);
+  root = binary_tree_rotate_left(root);
+  binary_tree_print(root);
+  printf("\n");
+
+  root->right->right = binary_tree_node(root->right, 450);
+  root->right->left = binary_tree_node(root->right, 420);
+  binary_tree_print(root);
+  printf("Rotate-left %d\n", root->n);
+  root = binary_tree_rotate_left(root);
+  binary_tree_print(root);
   return (0);
 }
