cbf1684bdd8118531f8f7ab1485292136d2d0d5b
Yeeloman <yami.onlyme@gmail.com>
2023-11-02 14:19:09 +0100

    add task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,16 +8,14 @@
  *Return: return a pointer to the sibling node or null
  */
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-{
-	if (!node->parent || !node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node) {
+	if (!node)
 		return (NULL);
-	if (!node->parent->left && !node->parent->right)
-		return (NULL);
-	if (node == node->parent->left)
-		return (node->parent->right);
-	else
+	if (node->parent && node->parent->left != node)
 		return (node->parent->left);
+	else if (node->parent && node->parent->right != node)
+		return (node->parent->right);
+	return (NULL);
 }
 
 /**
@@ -26,8 +24,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  *Return: uncle node or NULL
  */
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
+binary_tree_t *binary_tree_uncle(binary_tree_t *node) {
 	binary_tree_t *uncle;
 
 	uncle = malloc(sizeof(struct binary_tree_s));
