c43f81adef23f69ab1ba5e65f8088ffb699aa0f7
Yeeloman <yami.onlyme@gmail.com>
2023-11-02 14:52:18 +0100

    fix task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,23 +3,6 @@
 #include <stdlib.h>
 
 /**
- *binary_sibling - finds the sibling of a node
- *@node: a pointer to the node to find the sibling
- *Return: return a pointer to the sibling node or null
- */
-
-binary_tree_t *binary_sibling(binary_tree_t *node)
-{
-	if (!node)
-		return (NULL);
-	if (node->parent && node->parent->left != node)
-		return (node->parent->left);
-	else if (node->parent && node->parent->right != node)
-		return (node->parent->right);
-	return (NULL);
-}
-
-/**
  *binary_tree_uncle - finds the uncle of a node
  *@node: a pointer to the node to find the uncle
  *Return: uncle node or NULL
@@ -27,13 +10,11 @@ binary_tree_t *binary_sibling(binary_tree_t *node)
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *uncle;
-
-	uncle = malloc(sizeof(struct binary_tree_s));
-	if (!uncle)
+	if (!node ||
+	    !node->parent ||
+	    !node->parent->parent)
 		return (NULL);
-	if (!node)
-		return (NULL);
-	uncle = binary_sibling(node->parent);
-	return (uncle);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
